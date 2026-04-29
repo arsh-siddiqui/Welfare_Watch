61b49162a54e6491b5d9c7c648d36f0ea7e292b0
arsh-siddiqui <74855274+arsh-siddiqui@users.noreply.github.com>
2026-04-29 17:48:01 +0530

    Configure project for Render deployment

--- app.py
+++ app.py
@@ -1455,4 +1455,5 @@ with app.app_context():
     auto_import_2024_2026()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
